c373a7c021fba07b7b21d83a69d78bfbe0671a14
laren11 <laren.sedlanic@gmail.com>
2023-01-18 20:40:32 +0100

    Posiljanje emailov implementacija

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,43 +1,50 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import Rating from '@mui/material/Rating';
 import Box from '@mui/material/Box'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
   const [formStatus, setFormStatus] = useState('Send')
   const [rating, setRating] = useState(2);
+  const form = useRef();
+
   const onSubmit = (e) => {
     e.preventDefault()
     setFormStatus('Submitting...')
-    const { name, email, message } = e.target.elements
-    let conFom = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-      rating: rating,
-    }
-    console.log(conFom)
+
+    emailjs.sendForm(
+      "service_1vgi888",
+      "template_3pvrmea",
+      form.current,
+      "OFqvJNeXiCbFvLRCF"
+    ).then(
+      result => console.log(result.text), setFormStatus("Send"),console.log(form.current),
+      error => console.log(error.text)
+    )
+
+    e.target.reset();
   }
   return (
     <div className="container mt-5"  style={{height: '80vh'}}>
       <h2 className="mb-3">Contact us!</h2>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} ref={form}>
         <div className="mb-3">
           <label className="form-label" htmlFor="name">
             Name
           </label>
-          <input className="form-control" type="text" id="name" required />
+          <input className="form-control" type="text" name="name" id="name" required />
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="email">
             Email
           </label>
-          <input className="form-control" type="email" id="email" required />
+          <input className="form-control" type="email" name="email" id="email" required />
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="message">
             Message
           </label>
-          <textarea className="form-control" id="message" required />
+          <textarea className="form-control" name="message" id="message" required />
         </div>  
         <div className="mb-3">
           <Rating
